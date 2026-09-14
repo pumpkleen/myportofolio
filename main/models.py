0bbd49d197f3062b2991ced4bba95dfe0a04a52f
Cheryl Mahira <cherylmahiraaaa@gmail.com>
2026-09-14 20:18:34 +0700

    Tugas 2 - Menambahkan halaman Projects, Unit Test, dan CSS dengan MVT

--- main/models.py
+++ main/models.py
@@ -27,3 +27,13 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    link = models.URLField(blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Project
 
 
 def show_main(request):
@@ -21,3 +21,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_projects(request):
+    context = {
+        "name": "Mahi", 
+        "project_list": Project.objects.all(),
+    }
+    return render(request, "projects.html", context)
